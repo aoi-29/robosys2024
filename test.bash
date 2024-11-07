ce3a7501d70fe782c09268d1a10e47b5e31f604e
Aoi Takahashi <ao.kyrie.11@gmail.com>
2024-11-07 11:59:54 +0900

    Add acommand

--- test.bash
+++ test.bash
@@ -1,3 +1,22 @@
-#!/bin/bash
+#!/bin/bash -xv
 
+ng () {
+	echo ${1}行目が違う
+	res=1
+}
 
+res=0
+
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "$LINENO"
+
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK
+exit $res
