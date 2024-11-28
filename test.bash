ae3f012383b680f21f366e59ff9307d32adf0557
aoi-29 <ao.kyrie.11@gmail.com>
2024-11-29 03:49:39 +0900

    Update test.bash

--- test.bash
+++ test.bash
@@ -7,16 +7,18 @@ ng () {
 
 res=0
 
+chmod +x ./reverse_text
+
 test_reverse() {
-    local input = "$1"
-    local expected = "$2"
-    local output = $(echo "$input" | ./reverse_text)
+    local input="$1"
+    local expected="$2"
+    local output=$(echo "$input" | ./reverse_text)
     [ "$output" = "$expected" ] || ng "$LINENO"
 }
 
 test_reverse "hello" "olleh"
 test_reverse "12345" "54321"
-test_revrse "日本語" "語本日"
+test_reverse "日本語" "語本日"
 
 test_reverse "a b c" "c b a"
 test_reverse "  " "  "
