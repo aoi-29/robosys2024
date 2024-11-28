20111f19fde86bf731dddf7622863feab839e608
aoi-29 <ao.kyrie.11@gmail.com>
2024-11-29 03:43:57 +0900

    Update test.bash

--- test.bash
+++ test.bash
@@ -1,22 +1,30 @@
 #!/bin/bash -xv
 
 ng () {
-	echo ${1}行目が違う
+	echo "Error: ${1}行目が違う"
 	res=1
 }
 
 res=0
 
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng "$LINENO"
+test_reverse() {
+    local input = "$1"
+    local expected = "$2"
+    local output = $(echo "$input" | ./reverse_text)
+    [ "$output" = "$expected" ] || ng "$LINENO"
+}
 
-out=$(echo あ | ./plus)
-[ "$?" = 1 ]      || ng "$LINENO"
+test_reverse "hello" "olleh"
+test_reverse "12345" "54321"
+test_revrse "日本語" "語本日"
+
+test_reverse "a b c" "c b a"
+test_reverse "  " "  "
+test_reverse "!@#" "#@!"
+
+out=$(echo "" | ./reverse_text)
+[ "$?" = 1 ] || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
-out=$(echo | ./plus)
-[ "$?" = 1 ]      || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
-
-[ "$res" = 0 ] && echo OK
+[ "$res" = 0 ] && echo "All tests passed" || echo "Some tests failed"
 exit $res
